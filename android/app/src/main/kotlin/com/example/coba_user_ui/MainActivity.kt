f0576438bf4bfc4ad2b4d9e39d2925be734a4538
nikolaijan <posma2018@gmail.com>
2022-11-26 22:48:37 +0700

    oke

---
+++ android/app/src/main/kotlin/com/example/coba_user_ui/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.coba_user_ui
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,51 @@
+import 'package:coba_user_ui/provider/dark_theme_provider.dart';
+import 'package:coba_user_ui/screens/btm_bar.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'consts/theme_data.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  DarkThemeProvider themeChangeProvider = DarkThemeProvider();
+
+  void getCurrentAppTheme() async {
+    themeChangeProvider.setDarkTheme =
+    await themeChangeProvider.darkThemePrefs.getTheme();
+  }
+
+  @override
+  void initState() {
+    getCurrentAppTheme();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) {
+          return themeChangeProvider;
+        })
+      ],
+      child:
+      Consumer<DarkThemeProvider>(builder: (context, themeProvider, child) {
+        return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            title: 'Flutter Demo',
+            theme: Styles.themeData(themeProvider.getDarkTheme, context),
+            home: const BottomBarScreen());
+      }),
+    );
+  }
+}
